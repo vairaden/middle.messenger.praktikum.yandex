04fd73bdb3179ec006d89b401ee1fba369acc039
Boris Avgustinovich <74373178+vairaden@users.noreply.github.com>
2023-12-09 04:18:17 +0300

    Build and start fix

--- .eslintrc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  extends: 'airbnb',
-  parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
-  rules: {
-    'max-len': ['error', 100],
-    '@typescript-eslint/no-unused-vars': 'error',
-  },
-};
--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,10 @@
-import path from "path";
 import express from "express";
 
 const PORT = 3000;
 
 const app = express();
 
-app.use(express.static(path.resolve(__dirname, "dist")));
+app.use(express.static('dist'));
 
 app.listen(PORT, () => {
   console.log("Server started");
